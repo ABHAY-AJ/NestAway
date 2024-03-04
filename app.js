0b963a2edd14fe69fac38eb4df0e9fd47b3c9c32
ABHAY-AJ <krabhay476@gmail.com>
2024-03-05 00:56:20 +0530

    Bugs resolved

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ const User = require("./models/user.js");
 
 
 
-const dbUrl = process.env.ATLASDB_URL;
+// const dbUrl = process.env.ATLASDB_URL;
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
     .then(()=>{
@@ -35,7 +35,7 @@ main()
     })
 
 async function main(){
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
+    mongoUrl: MONGO_URL,
     crypto: {
       secret: process.env.SECRET,
     },
